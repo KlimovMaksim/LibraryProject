b8ec2437f6a03265004c4f6d32007d9fcaf35106
Maksim Klimov <maksimklimov779@gmail.com>
2023-11-22 01:48:30 +0300

    lesson 24: validation, @Valid

--- src/main/java/ru/klimov/springmvc/models/Person.java
+++ src/main/java/ru/klimov/springmvc/models/Person.java
@@ -1,15 +1,33 @@
 package ru.klimov.springmvc.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class Person {
     private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
+
+    @NotEmpty(message = "Age should not be empty")
+    @Min(value = 0, message = "Age should be greater than 0")
+    private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
+    private String email;
 
     public Person() {
     }
 
-    public Person(int id, String name) {
+    public Person(int id, String name, int age, String email) {
         this.id = id;
         this.name = name;
+        this.age = age;
+        this.email = email;
     }
 
     public int getId() {
@@ -20,11 +38,27 @@ public class Person {
         return name;
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
     public void setId(int id) {
         this.id = id;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
